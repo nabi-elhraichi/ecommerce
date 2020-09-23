e5f53750f4aaf7a1f2ed8ee7238f13b9a190c8d8
nabi-elhraichi <nabil.elhraichi@edu.umi.ac.ma>
2020-09-23 22:11:04 +0200

    ecommerce test with django

---
+++ products/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from products.models import Product
+
+class Home(ListView):
+    model = Product
+    template_name = 'products/home.html'
